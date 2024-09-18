20cca32c05d0f924e04dea8f3a3e5f678fd60592
pataliu <pataliu@gmail.com>
2024-09-18 12:05:04 +0800

    v2.3.3

--- src/spritesheet/templates/SCSSTemplatePlugin.ts
+++ src/spritesheet/templates/SCSSTemplatePlugin.ts
@@ -36,9 +36,11 @@ export default class SCSSTemplatePlugin {
     const imagePath = path.relative(target.css, target.image);
     const { properties, coordinates } = result;
     const names = Object.keys(coordinates)
+    const sourceName = path.basename(spritesheet.source)
     let tpl = '@use "sass:map";\n'
     // 注入每个图片，一个大的Map，然后从中取值。
     tpl += `
+    $${this.prefix}_createtime: '${new Date().toLocaleString()}';
     // nested map.
     $${this.prefix}: (
       ${names.map(k => {
@@ -57,25 +59,38 @@ export default class SCSSTemplatePlugin {
     );\n`
     // 注入mixins
     tpl += `
-    @mixin spritesheet($name) {
+    @mixin ${sourceName}-spr($name, $scale: 1) {
+      @if map.has-key($${this.prefix}, $name) {
+      } @else {
+        @error "${sourceName} 中未找到 #{$name}";
+      }
       $spr-width: map.get($${this.prefix}, $name, "width");
       $spr-height: map.get($${this.prefix}, $name, "height");
       $spr-image: map.get($${this.prefix}, $name, "image");
-      $spr-offset-x: map.get($${this.prefix}, $name, "x");
-      $spr-offset-y: map.get($${this.prefix}, $name, "y");
-      width: ${this.transformUnit('$spr-width')};
-      height: ${this.transformUnit('$spr-height')};
+      $spr-offset-x: map.get($${this.prefix}, $name, "x") * -1;
+      $spr-offset-y: map.get($${this.prefix}, $name, "y") * -1;
+      width: ${this.transformUnit('$spr-width')} * $scale;
+      height: ${this.transformUnit('$spr-height')} * $scale;
       background-image: url("#{$spr-image}");
-      background-position: ${this.transformUnit('$spr-offset-x')} ${this.transformUnit('$spr-offset-y')};
-      background-size: ${this.transformUnit(`${properties.width}px`)} ${this.transformUnit(`${properties.height}px`)};
+      background-position: ${this.transformUnit('$spr-offset-x')} * $scale ${this.transformUnit('$spr-offset-y')} * $scale;
+      background-size: ${this.transformUnit(`${properties.width}px`)} * $scale ${this.transformUnit(`${properties.height}px`)} * $scale;
     }
-    `
-
-    // test
-    tpl += `
-    .arrow {
-      @include spritesheet('arrow')
-    }`
+    @mixin ${sourceName}-spr-mask($name, $scale: 1) {
+      @if map.has-key($${this.prefix}, $name) {
+      } @else {
+        @error "${sourceName} 中未找到 #{$name}";
+      }
+      $spr-width: map.get($${this.prefix}, $name, "width");
+      $spr-height: map.get($${this.prefix}, $name, "height");
+      $spr-image: map.get($${this.prefix}, $name, "image");
+      $spr-offset-x: map.get($${this.prefix}, $name, "x") * -1;
+      $spr-offset-y: map.get($${this.prefix}, $name, "y") * -1;
+      -webkit-mask-image: url("#{$spr-image}");
+      -webkit-mask-position: ${this.transformUnit('$spr-offset-x')} * $scale ${this.transformUnit('$spr-offset-y')} * $scale;
+      -webkit-mask-size: ${this.transformUnit(`${properties.width}px`)} * $scale ${this.transformUnit(`${properties.height}px`)} * $scale;
+    }
+    `;
+    
 
     tpl = beautify.css(tpl, { end_with_newline: true, indent_size: 2 });
     return tpl
